6cf9615caba31f0525bd618d7d02f16b88a32b51
Djioleu Florian <nsiegningflorian@gmail.com>
2024-03-23 21:11:51 +0100

    Add the Modal component structure

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useEffect, useRef, useState } from "react";
+import { ReactNode, useRef, useState } from "react";
 
 type ModalTypes = {
   children: ReactNode;
@@ -9,27 +9,6 @@ type ModalTypes = {
 const Modal = ({ children, open, handleModal }: ModalTypes) => {
   const modalRef = useRef<HTMLDivElement>(null);
   const [openModal, setOpenModal] = useState<boolean>(open);
-  /* const hModal = useCallback(
-    (state: boolean) => handleModal(state),
-    [handleModal]
-  ); */
-
-  useEffect(() => {
-    const e = document.addEventListener("click", (event: MouseEvent) => {
-      if (
-        modalRef.current &&
-        !modalRef.current.contains(event.target as Node)
-      ) {
-        setOpenModal((openModal) => !openModal);
-        handleModal(openModal);
-      }
-    });
-    return () => document.removeEventListener("click", e);
-  }, [handleModal, openModal]);
-
-  useEffect(() => {
-    setOpenModal(() => open);
-  }, [open]);
 
   const modal = openModal && (
     <div ref={modalRef} className="px-8 py-8 shadow-md rounded-md">
